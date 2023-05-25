d296468678d1651c0ce574adf9353a3e56542c9a
gabriel@indicium.tech <gabriel@indicium.tech>
2023-05-25 23:06:00 +0000

    Changing stg_orders.sql

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,14 +1,10 @@
-{{config(materialized='incremental', unique_key = 'order_id')}}
-
 with
     orders as (
         select *
         from {{source('northwind', 'orders')}}
         {% if env_var("DBT_TARGET_SCHEMA") == 'dev' %}
-
   -- this filter will only be applied to the development schema
   limit 100
-
         {% endif %}
     )
 
